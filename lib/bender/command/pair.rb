f384af2dcac19d37d1fc4260adb61917349d3a6f
Jaymes Waters and Nick Karpenske <dev+jaym3s+randland@bendyworks.com>
2011-01-07 14:38:36 -0600

    Add bender pair <name> <name>

--- lib/bender/command/pair.rb
+++ lib/bender/command/pair.rb
@@ -3,20 +3,35 @@ require 'bender/command/base'
 module Bender::Command
   class Pair < Base
 
+    class << self
+      def help_summary
+        "Lord and Overseer of all things that pair"
+      end
+    end
+
     def default
-      pair_info = hitch_info
-      if pair_info
-        puts pair_info
+      if hitch_available?
+        case @args.size
+        when 0
+          puts pair_info
+        else
+          if @args[0] == '-u' || @args[0] == '--unpair'
+            unpair
+          else
+            pair @args
+          end
+        end
       else
         puts hitch_error
       end
     end
 
-    protected
+  protected
 
     def hitch_available?
       begin
         require 'hitch'
+        @pair_class = Hitch
         true
       rescue LoadError
         false
@@ -24,20 +39,55 @@ module Bender::Command
     end
 
     def hitch_names
-      Hitch.send(:git_author_name)
+      @pair_class.send(:git_author_name)
     end
 
     def hitch_emails
-      Hitch.send(:git_author_email)
+      @pair_class.send(:git_author_email)
     end
 
     def hitch_info
-      hitch_available? ? "#{hitch_names} <#{hitch_emails}>" : nil
+      "#{hitch_names} <#{hitch_emails}>"
+    end
+
+    def unpair
+      case @pair_class.name
+      when "Hitch"
+        @pair_class.send(:unhitch)
+      else
+        nil
+      end
+    end
+
+    def pair args
+      case @pair_class.name
+      when "Hitch"
+        @pair_class.send(:export, args)
+      else
+        nil
+      end
+    end
+
+    def pair_info
+      case @pair_class.name
+      when "Hitch"
+        hitch_info
+      else
+        nil
+      end
+    end
+
+    def pair_error
+      case @pair_class.name
+      when "Hitch"
+        hitch_error
+      else
+        nil
+      end
     end
 
     def hitch_error
-      'No pair info available!\n  (add hitch to your project)\n\n --OR-- \n\n fork me and hack away!'
+      "No pair info available!\n\n  add hitch to your project\n     --OR-- \n  fork me and hack away!"
     end
-
   end
 end
--- spec/commands/pair_spec.rb
+++ spec/commands/pair_spec.rb
@@ -3,15 +3,25 @@ require File.expand_path(File.join(__FILE__, '../../../', 'lib/bender/command/pa
 
 describe Bender::Command::Pair do
   let(:args) { [] }
-  subject {Bender::Command::Pair.new(args)}
+  subject { Bender::Command::Pair.new(args) }
+
+  before do
+    subject.instance_variable_set(:@pair_class, stub('Hitch', :name => 'Hitch'))
+  end
+
+  describe 'actions' do
+    its(:class) { should respond_to(:help_summary) }
+
+    it { should respond_to(:default) }
+  end
 
   describe '#default' do
+    let(:test_class) { subject.class.any_instance }
     let(:hitch_available) { false }
     let(:names) { "Jaymes Waters and Nick Karpenske" }
     let(:emails) { "dev+jaym3s+randland@bendyworks.com" }
     let(:hitch_info) { "#{names} <#{emails}>" }
     let(:hitch_error) { 'error' }
-    let(:test_class) { subject.class.any_instance }
 
     before do
       test_class.stubs(:hitch_available?).returns(hitch_available)
@@ -27,36 +37,46 @@ describe Bender::Command::Pair do
       test_class.expects(:hitch_available?)
     end
 
-    context 'when hitch is available' do
-      let(:hitch_available) { true }
-
-
-      it 'should retrieve the git pair names' do
-        test_class.expects(:hitch_names)
-      end
-
-      it 'should retrieve the git pair emails' do
-        test_class.expects(:hitch_emails)
-      end
-
-      it 'should print a formatted pair info string' do
-        test_class.expects(:puts).with(hitch_info)
-      end
-    end
-
     context 'when hitch is not available' do
       let(:hitch_available) { false }
 
-      it 'should not retrieve the git pair names' do
-        test_class.expects(:hitch_names).never
-      end
-
-      it 'should not retrieve the git pair emails' do
-        test_class.expects(:hitch_emails).never
-      end
-
       it 'should print an error message' do
         test_class.expects(:puts).with(hitch_error)
+      end
+    end
+
+    context 'when hitch is available' do
+      let(:hitch_available) { true }
+
+      context 'and no parameters are given' do
+        let(:args) { [] }
+        it 'should retrieve the git pair names' do
+          test_class.expects(:hitch_names)
+        end
+
+        it 'should retrieve the git pair emails' do
+          test_class.expects(:hitch_emails)
+        end
+
+        it 'should print a formatted pair info string' do
+          test_class.expects(:puts).with(hitch_info)
+        end
+      end
+
+      context 'and the -u parameter is given' do
+        let(:args) { %w(-u) }
+
+        it 'should unpair' do
+          test_class.expects(:unpair)
+        end
+      end
+
+      context 'and name parameters are given' do
+        let(:args) { ["jaym3s","randland"] }
+
+        it 'should pair the two users' do
+          test_class.expects(:pair).with(args)
+        end
       end
     end
   end
